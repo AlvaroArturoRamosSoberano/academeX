8c136653260411e6a469a1ccebc4e77f49239996
AlvaroArturoRamosSoberano <alvarors935@gmail.com>
2024-02-01 15:07:47 -0600

    Add resources, migrations, models, requests, controllers

---
+++ app/Http/Controllers/EnnrollmentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ennrollment;
+use App\Http\Requests\ennrollments\StoreEnnrollmentRequest;
+use App\Http\Requests\ennrollments\UpdateEnnrollmentRequest;
+
+class EnnrollmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEnnrollmentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Ennrollment $ennrollment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Ennrollment $ennrollment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEnnrollmentRequest $request, Ennrollment $ennrollment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Ennrollment $ennrollment)
+    {
+        //
+    }
+}
